78d4d9f7ed7ebf72cd66f10d5a54e9cf0b7e4c46
akioyokota <y.akio1910@gmail.com>
2020-06-22 20:46:52 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.14.1"
 
 set :application, "chat-space"
-set :repo_url, "git@example.com:akioyokota/chat-space-.git.git"
+set :repo_url, "git@example.com:akioyokota/chat-space.git.git"
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
